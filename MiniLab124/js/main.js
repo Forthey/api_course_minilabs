6683fe6961f2523a2ccb4e783dffbe2d7bb1f8ee
Nikita <0forthey0@gmail.com>
2024-11-06 16:06:18 +0300

    Implement lab1 base tasks

--- MiniLab124/js/main.js
+++ MiniLab124/js/main.js
@@ -1,75 +1,82 @@
-import {setFormValue, submitSignUpForm, validateEmail, validatePassword} from "./utils.js"
-
-
-////// ДЕМОНСТРАЦИОННЫЙ УЧАСТОК КОДА. На оценку не влияет, исключительно для саморазвития.
-
-// Предлагаю "поиграться" с частями кода ниже, чтобы познакомиться с JS
-// Получаем элемент и меняем его класс, который определен в библиотеке стилей materialize
-const password = document.getElementById('password');
-password.classList.add("valid")
-password.classList.remove("valid")
-
-// В браузере можно посмотреть, что из себя представляет документ
-// (CTRL+SHIFT+i для открытия консоли и открыть вкладку "консоль", туда будет залогированно значение)
-console.log("Document")
-console.log(document)
-
-// Если запросить id, которого нет в DOM дереве - вернется undefined
-// => надо быть осторожней: коллега может поменять id вашего элемента и упадёт !ВАШ! код
-// const first_name = document.getElementById('first_name_invalid');
-// first_name.oninput = (e) => validatePassword(e)
-
-// Селекция по классу. Может пригодится, для того, чтобы упростить обработку полей в двух формах.
-// Чтобы не делать кучу уникальных айди, можно определённым полям формы давать один класс и обрабатывать их в цикле
-// const passwords = document.querySelectorAll('.password')
-// console.log(passwords)
-// for (const password of passwords) {
-//   password.style.background = "red"
-// }
-
-////// КОНЕЦ ДЕМОНСТРАЦИОННОГО УЧАСТКА КОДА. Дальше код для оцениваемой части задания
-
+import {
+    clearSignForm,
+    customValidator, getValidationStatus,
+    setFormValue,
+    submitSignForm,
+    validateEmail,
+    validatePassword,
+    validateRepeatPassword
+} from "./utils.js"
 
 // Выписываем все айдишники HTMl-элементов в константы для переиспользования
 const first_name_id = 'first_name'
 const last_name_id = 'last_name'
-const password_id = 'password'
-const email_id = 'email'
+const password_class = 'password'
+const password_repeat_id = 'password-repeat'
+const email_class = 'email'
 
-const sign_in_link_id = 'sign_in_link'
+const sign_button_class = 'sign_button'
 const sign_up_form_id = 'sign_up_form'
-// const sign_in_form_id = 'sign_in_form'  // Пригодится
-const sign_up_btn_id = 'sign_up_btn'
+const sign_in_link_id = 'sign_in_link'
+
 const sign_in_form_id = 'sign_in_form'
+const sign_up_link_id = 'sign_up_link'
 
+const sign_in_form = document.getElementById(sign_in_form_id).getElementsByTagName('form')[0]
+const first_name = document.getElementById(first_name_id)
+const last_name = document.getElementById(last_name_id)
+const email_fields = document.getElementsByClassName(email_class)
+const password_fields = document.getElementsByClassName(password_class)
+const password_repeat = document.getElementById(password_repeat_id)
+const sign_buttons = document.getElementsByClassName(sign_button_class)
+const switch_to_sign_in = document.getElementById(sign_in_link_id)
 
-// Получаем элемент DOM-дерева по id и присваиваем значение аттрибуту oninput
-// oninput вызывается с параметром "event" каждый раз, когда ввод меняется
-// Значение, которое мы присваеваем этому аттрибуту - это функция, определённая в стрелочном стиле
-// Гуглить по тегам "события JS", "onchange/oninput HTML", "стрелочные функции JS", ...
+const sign_up_form = document.getElementById(sign_up_form_id).getElementsByTagName('form')[0]
+const switch_to_sign_up = document.getElementById(sign_up_link_id)
 
-const first_name = document.getElementById(first_name_id);
-first_name.oninput = (e) => setFormValue(first_name_id, e.target.value)  // Установить значение без валидации
+first_name.oninput = (e) => setFormValue(first_name_id, e.target.value)
 
-const email = document.getElementById(email_id);
-email.oninput = (e) => setFormValue(email_id, e.target.value, validateEmail) // Установить значение с валидацией
+last_name.oninput = (e) => setFormValue(last_name_id, e.target.value)
 
-
-
-// Меняем стили объекта DOM дерева. Это позволяет скрыть форму регистрации и показать форму авторизации
-// Объект формы не исключается из DOM дерева, а просто становистя невидимым
-const switch_to_sign_in = document.getElementById(sign_in_link_id);
-switch_to_sign_in.onclick = (e) => {
-  document.getElementById(sign_up_form_id).style.display = "none"
-  document.getElementById(sign_in_form_id).style.display = ""
+for (let i = 0; i < email_fields.length; i++) {
+    const email = email_fields.item(i)
+    email.oninput = (e) => setFormValue(email_class, e.target.value, validateEmail)
+    email.onblur = (_) => {
+        sign_buttons.item(i).disabled = !getValidationStatus()
+    }
 }
 
+for (let i = 0; i < password_fields.length; i++) {
+    const password = password_fields.item(i)
+    password.oninput = (e) => setFormValue(password_class, e.target.value, validatePassword)
+    password.onblur = (e) => {
+        customValidator(password, validatePassword, e.target.value)
+        sign_buttons.item(i).disabled = !getValidationStatus()
+    }
 
-const sign_up_btn = document.getElementById(sign_up_btn_id);
-sign_up_btn.onclick = (e) => {
-  // При нажатии кнопки в форме по умолчанию происходит перезагрузка страницы.
-  // Чтобы отключить его, нужно отменить стандартное поведение события
-  e.preventDefault()
-  submitSignUpForm()
+    sign_buttons.item(i).onclick = (e) => {
+        e.preventDefault()
+        submitSignForm()
+    }
+}
+
+password_repeat.oninput = (e) => setFormValue(password_repeat_id, e.target.value, validateRepeatPassword)
+password_repeat.onblur = (e) => {
+    customValidator(password_repeat, validateRepeatPassword, e.target.value)
+    sign_buttons.item(0).disabled = !getValidationStatus()
+}
+
+switch_to_sign_in.onclick = (e) => {
+    sign_up_form.reset()
+    clearSignForm()
+    document.getElementById(sign_up_form_id).style.display = "none"
+    document.getElementById(sign_in_form_id).style.display = ""
+}
+
+switch_to_sign_up.onclick = (e) => {
+    sign_in_form.reset()
+    clearSignForm()
+    document.getElementById(sign_in_form_id).style.display = "none"
+    document.getElementById(sign_up_form_id).style.display = ""
 }
 
--- MiniLab124/js/utils.js
+++ MiniLab124/js/utils.js
@@ -1,23 +1,49 @@
-const formValues = {}  // Сюда пишутся значения формы (Object как в Java, или dict из Python)
-const formValidation = {}  // Сюда пишутся статусы валидации каждого поля. Если поле ни разу не валидировалось,
-// то при обращении к Object вернётся undefined, который при логическом сравнении обрабатывается как false
+const formValues = {}
+const formValidation = {}
 
 
-// Объявляется и инициализируется константная переменная
-// Инициализация функцией, заданной в стрелочном виде
-export const validatePassword = (e) => {
-  formValidation.password = e.target.value
-  console.log("Password validation...")
-  console.log(e)
-  // Напишите код валидации здесь и присвойте true/false в объект(словарь) formValidation
-  // formValidation.password = ...  // formValidation['password'] = ... - то же самое, но другой синтаксис
-  return formValidation.password !== undefined   // Это заглушка, return вероятно надо переписать
+
+/**
+ *
+ * @param {HTMLElement} htmlElement
+ * @param {Function} validator
+ * @param {string} value
+ */
+export const customValidator = (htmlElement, validator, value) => {
+  if (validator(value)) {
+    htmlElement.classList.add("valid");
+    htmlElement.classList.remove("invalid");
+  } else {
+    htmlElement.classList.add("invalid");
+    htmlElement.classList.remove("valid");
+  }
+}
+
+/**
+ * @param {string} password
+ * @return {RegExpMatchArray}
+ */
+export const validatePassword = (password) => {
+  const regExp = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+
+  return password.match(regExp)
+}
+
+/**
+ *
+ * @param {string} password
+ * @return {boolean}
+ */
+export const validateRepeatPassword = (password) => {
+  return formValues["password"] === password
 }
 
 
+/**
+ * @param {string} email
+ * @returns {RegExpMatchArray}
+ */
 export const validateEmail = (email) => {
-  // Создадим шаблон регулярного выражения. В нём применяются шаблонные строки
-  // Гуглить по тегам: "шаблонные строки js", "регулярные выражения"
   const regExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
   return String(email)
@@ -26,16 +52,11 @@ export const validateEmail = (email) => {
 }
 
 
-// Функция возвращающая true если все валидации пройдены, и false если хотя бы одна не пройдена
 export const getValidationStatus = () => {
-  // Происходит функциональная мгаия, читай строчку кода ниже как:
-  // Получить значения (не ключи) из объекта, затем применить к каждому значению функцию двойного логического отрицания
-  // (преобразование к булевому типу) и результаты всех применений это true, то вернуть true, иначе - false
   return Object.values(formValidation).every((validationStatus) => !!validationStatus)
 }
 
 
-// Функция возвращающая которая ставит значение поля в форме по ключу
 export const setFormValue = (valueKey, newValue, validator) => {
   formValues[valueKey] = newValue
   if (validator !== undefined) {
@@ -44,14 +65,25 @@ export const setFormValue = (valueKey, newValue, validator) => {
 }
 
 
-// Функция для обработки отправки формы регистрации
-// В этой функции должен быть http запрос на сервер для регистрации пользователя (сейчас просто демонстрация)
-export const submitSignUpForm = () => {
+export const submitSignForm = () => {
+  console.log(formValues)
   if (!getValidationStatus()) {
     console.log("FORM IS INCORRECT")
     return false
   }
   console.log("FORM IS FINE")
-  console.log(formValues)
   return true
+}
+
+export const clearSignForm = () => {
+  for (let field in formValues) {
+    if (formValues.hasOwnProperty(field)) {
+      delete formValues[field]
+    }
+  }
+  for (let field in formValidation) {
+    if (formValidation.hasOwnProperty(field)) {
+      delete formValidation[field]
+    }
+  }
 }
